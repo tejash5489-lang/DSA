cda30a4690f33137b6f7c261502befb13799efcf
Tejash Gupta <tejash5489@gmail.com>
2026-07-28 19:02:11 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (75.00%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int dup=-1,miss=-1;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[abs(nums[i])-1]<0) dup=abs(nums[i]);
+            else{
+                nums[abs(nums[i])-1]*=(-1);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                miss=(i+1);
+                break;
+            }
+        }
+        return {dup,miss};
+    }
+};
