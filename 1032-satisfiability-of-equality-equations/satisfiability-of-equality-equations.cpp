3475e2b55faa10aba3ec5f133653d8b37a6d38fd
Tejash Gupta <tejash5489@gmail.com>
2026-08-09 22:00:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (56.89%) - LeetSync

---
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x){
+        if(x==parent[x])
+            return x;
+
+        return parent[x]=find(parent[x]);
+    }
+
+    void Union(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+
+        if(x_parent==y_parent)
+            return ;
+
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }
+        else if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }
+        else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+    bool equationsPossible(vector<string>& equations) {
+        parent.resize(26,0);
+        rank.resize(26,0);
+
+        for(int i=0;i<26;i++){
+            parent[i]=i;
+        }
+
+        for(string& s:equations){
+            if(s[1]=='='){
+                Union(s[0]-'a',s[3]-'a');
+            }
+        }
+
+        for(string& s:equations){
+            if(s[1]== '!'){
+                char first=s[0];
+                char second=s[3];
+
+                int parent_f=find(first-'a');
+                int parent_s=find(second-'a');
+
+                if(parent_f==parent_s)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
