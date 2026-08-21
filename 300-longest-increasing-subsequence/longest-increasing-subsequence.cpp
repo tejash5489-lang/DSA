fd1a045f30613727eabf08df18c4e48329f8ee66
Tejash Gupta <tejash5489@gmail.com>
2026-08-22 01:05:07 +0530

    Time: 131 ms (39.98%) | Memory: 14.5 MB (41.16%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int n, int curr, int prev,
-              vector<vector<int>>& dp) {
-        // base case
-        if (curr == n)
-            return 0;
-
-        if (dp[curr][prev + 1] != -1)
-            return dp[curr][prev + 1];
-
-        // include
-        int take = 0;
-        if (prev == -1 || nums[curr] > nums[prev])
-            take = 1 + solve(nums, n, curr + 1, curr, dp);
-
-        // exclude
-        int nottake = 0 + solve(nums, n, curr + 1, prev, dp);
-
-        // return the max
-        return dp[curr][prev + 1] = max(take, nottake);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(nums, n, 0, -1, dp);
+        vector<int> currrow(n + 1, 0);
+        vector<int> nextrow(n + 1, 0);
+
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1; prev--) {
+
+                int take = 0;
+                if (prev == -1 || nums[curr] > nums[prev])
+                    take = 1 + nextrow[curr + 1];
+
+                int nottake = 0 + currrow[prev + 1];
+                currrow[prev+1]=max(take,nottake);
+            }
+            nextrow = currrow;
+        }
+        return nextrow[0];
     }
 };
