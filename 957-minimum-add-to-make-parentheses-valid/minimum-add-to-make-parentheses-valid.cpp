7e57873cbb4bc788903e4336d0b7556f67cc1ae7
Tejash Gupta <tejash5489@gmail.com>
2026-09-02 01:38:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (57.20%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int size = 0;
+        int open = 0;
+
+        for (char &ch : s) {
+            if (ch == '(')
+                size++;
+
+            else if (size > 0)
+                size--;
+
+            else
+                open++;
+
+        }
+        return size + open;
+    }
+};
