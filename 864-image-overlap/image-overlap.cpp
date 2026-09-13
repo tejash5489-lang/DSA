0d1c4bc8155f4b7201e31fc8db3ac8f641b1dd46
Tejash Gupta <tejash5489@gmail.com>
2026-09-13 19:06:07 +0530

    Time: 33 ms (84.75%) | Memory: 13 MB (68.83%) - LeetSync

---
+++ 864-image-overlap/image-overlap.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shiftAndCount(vector<vector<int>>& A, vector<vector<int>>& B, int x_shift, int y_shift) {
+        int n = A.size();
+        int leftShiftCount  = 0;
+        int rightShiftCount = 0;
+
+        int A_row = 0;
+
+        for(int B_row = x_shift; B_row<n; B_row++) {
+            int A_col = 0;
+            for(int B_col = y_shift; B_col<n; B_col++) {
+                if(B[B_row][B_col] == 1 && B[B_row][B_col] == A[A_row][A_col])
+                    leftShiftCount++;
+                if(B[B_row][A_col] == 1 && B[B_row][A_col] == A[A_row][B_col])
+                    rightShiftCount++;
+                A_col++;
+            }
+            A_row++;
+        }
+        return max(leftShiftCount, rightShiftCount);
+    }
+
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int maxOverlap = 0;
+        for(int x_shift = 0; x_shift<n; x_shift++) {
+            for(int y_shift = 0; y_shift<n; y_shift++) {
+                //Fix A, move B left and right
+                maxOverlap = max(maxOverlap, shiftAndCount(img1, img2, x_shift, y_shift));
+                //Fix B, move A
+                maxOverlap = max(maxOverlap, shiftAndCount(img2, img1, x_shift, y_shift));
+            }
+        }
+        return maxOverlap;
+    }
+};
