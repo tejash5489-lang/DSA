78e9e39375707eebc023c3163d9d6617d94dd585
Tejash Gupta <tejash5489@gmail.com>
2026-09-25 12:55:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (44.74%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -26,13 +26,14 @@ public:
             for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
+
                 if (node->left)
                     q.push(node->left);
                 if (node->right)
                     q.push(node->right);
+
                 level.push_back(node->val);
             }
-
             ans.push_back(level);
         }
         return ans;
